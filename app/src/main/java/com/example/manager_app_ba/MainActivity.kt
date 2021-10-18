cd204a7d89adf95dafdb76060b538ec06e6758f8
ssano <kssho3364@gmail.com>
2021-10-18 16:22:59 +0900

    211018_KSH
    ready!

--- app/src/main/java/com/example/manager_app_ba/MainActivity.kt
+++ app/src/main/java/com/example/manager_app_ba/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.manager_app_ba
 
+import android.app.ActivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.manager_app_ba.databinding.ActivityMainBinding
+import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private val mDatabase = FirebaseDatabase.getInstance().reference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mDatabase.child("User").child("admin").child("ID").addListenerForSingleValueEvent(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                var nameList =""
+                for (dataSnapshot : DataSnapshot in snapshot.children){
+                    Log.d("is work?",dataSnapshot.getValue().toString())
+                    nameList = nameList+dataSnapshot.getValue().toString()
+                }
+                binding.newText.setText(nameList)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+
+
+
+
+
     }
 }
